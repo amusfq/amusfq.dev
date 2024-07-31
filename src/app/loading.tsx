03ebc9e0850ebbb96d720e983882eafb1c0b96d6
Achmad Musyaffa Taufiqi <amusfq@gmail.com>
2024-08-01 03:05:25 +0700

    Add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className='py-32'>
+      <h3 className='text-2xl font-bold text-white text-center'>Loading...</h3>
+    </div>
+  )
+}
+
+export default Loading;
